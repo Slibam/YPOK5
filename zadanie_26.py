3ca39fa088d9e736ada08e0a1262e0437e5633b4
Oleg <proverkail@mail.ru>
2023-03-10 19:19:25 +0300

    1

---
+++ zadanie_26.py
@@ -0,0 +1,10 @@
+#Напишите программу, которая на вход принимает два числа A и B,
+#и возводит число А в целую степень B с помощью рекурсии.
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Введите число: "))
+exp = int(input("Введите его степень: "))
+print(f"Результат возведения {base} в степень {exp} равен ->", power(base, exp))
